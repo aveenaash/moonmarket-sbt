b74d82ea0ad162f82cf459d4890cd78f54480648
abinash <abinash.bastola@gmail.com>
2014-04-30 15:40:45 +0545

    ADD response update feature for existing request

--- src/main/scala/code/snippet/ElasticsearchManager.scala
+++ src/main/scala/code/snippet/ElasticsearchManager.scala
@@ -14,6 +14,8 @@ import net.liftweb.http.js.JE.JsArray
 import com.fasterxml.jackson.databind._
 import java.util
 import java.io.StringWriter
+import java.lang.Object
+import java.util.UUID
 
 //import json.{ Js, JsObject }
 import org.elasticsearch.client.transport.TransportClient
@@ -24,7 +26,12 @@ import org.elasticsearch.common.transport.InetSocketTransportAddress
 import org.elasticsearch.action.index.IndexRequestBuilder
 import org.elasticsearch.common.xcontent.XContentBuilder
 import org.elasticsearch.common.xcontent.XContentFactory
-import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.index.IndexResponse
+import org.elasticsearch.action.update.UpdateRequestBuilder
+import org.elasticsearch.index.query.QueryBuilders
+import org.elasticsearch.search.facet.FacetBuilders
+import org.elasticsearch.search.SearchHit
+import org.elasticsearch.search.facet.terms.TermsFacet
 
 object ElasticsearchManager {
 
@@ -63,48 +70,93 @@ object ElasticsearchManager {
     }
 
 
+  def getId(index : String ,request : String) : String = {
+      val termQueryBuilder= QueryBuilders.termQuery("request",request)
+      println("Request ::::" + request)
+
+      val termsFacet = FacetBuilders.termsFacet("tag").field("requestId");
+
+      val searchResponse =getClient().prepareSearch(index)
+                                     .setQuery(termQueryBuilder)
+                                     .addFacet(termsFacet)
+                                     .execute().actionGet()
+
+    var f  =  searchResponse.getFacets().facetsAsMap().get("tag");
+    var fterms : TermsFacet = f.asInstanceOf[TermsFacet]
+    var result : Array[SearchHit] = searchResponse.getHits().getHits()
+    var id : String  =""
+    if(result.length>0)
+    {
+      if(fterms.getTotalCount()>0)
+      {
+        id = result(0).getId()
+        println(id)
+      }
+
+    }
+    println(id)
+
+    return id
+
+        }
+
     def insertResponseRecord(requestdoc_ : Map[String,String],responsedoc_ : Map[String,String], index : String , type_ : String) = {
       var client=getClient()
+      var id =getId(index,requestdoc_.get("request").mkString)
+      println(id)
+      if(id=="")
+      {
+        println("Insert:::"+id)
+            val json = XContentFactory.jsonBuilder();
+            json.startObject();
 
-      val json = XContentFactory.jsonBuilder();
-      json.startObject();
+            requestdoc_.foreach { keyValue =>
+              val fieldName  = keyValue._1;
+              val fieldValue = keyValue._2;
 
-      requestdoc_.foreach { keyValue =>
-        val fieldName  = keyValue._1;
-        val fieldValue = keyValue._2;
-
-        json.field(fieldName, fieldValue)
-      }
-        json.startObject("responses")
+              json.field(fieldName, fieldValue)
+            }
+            json.startObject("responses")
             responsedoc_.foreach { keyValue =>
               val fieldName  = keyValue._1;
               val fieldValue = keyValue._2;
 
               json.field(fieldName, fieldValue)
-              }
-              json.endObject()
-      json.endObject()
+            }
+            json.endObject()
+            json.endObject()
+
+        val indexRequestBuilder = client.prepareIndex(index, type_)
+        indexRequestBuilder.setCreate(false).setSource(json);
+        indexRequestBuilder.execute().actionGet()
+
+      }
+      else {
+        val updateObject = new util.HashMap[String , Object]
+        var map = new util.HashMap[String, String]
+        map.put("responseId", "1");
+        map.put("response", responsedoc_.get("response").toString());
 
 
-//      val objectmapper: ObjectMapper = new ObjectMapper()
-//      objectmapper.configure(SerializationFeature.INDENT_OUTPUT, true)
 
-//      var stringEmp = new StringWriter();
-//      objectmapper.writeValue(stringEmp, hulaki);
-//      println("Employee JSON is\n"+stringEmp.toString);
+//        var reqids : Array[util.Map[String, String]] = new Array[util.Map[String, String]](2)
+//        reqids(0) = map
+//        reqids(1) = map_
+        println("Update bhitra:::" + id)
+        updateObject.put("responses", map);
 
-      //var jsonBuilder : String = objectmapper.writeValueAsString(jsonmap)
-     // println("JSON STRING ; "+jsonBuilder);
+        val updateRequestBuilder =client.prepareUpdate(index, type_, id)
+        updateRequestBuilder.setScript("ctx._source.responses += responses;")
+          .setScriptParams(updateObject).execute().actionGet();
 
-//      val indexRequestBuilder = client.prepareIndex(index, type_)
-//      indexRequestBuilder.setCreate(false).setSource(json);
-//      indexRequestBuilder.execute().actionGet()
 
-      println("builder:" + json.bytes().toUtf8().toString())
+      }
+
+     //println("builder => " + updateRequestBuilder.toString())
 
 
       //need to refresh indices to reflect the insertion made
-//      client.admin().indices().prepareRefresh().execute().actionGet()
+      client.admin().indices().prepareRefresh().execute().actionGet()
       true
 
 
